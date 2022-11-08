e02ec5231fbf1d3b48523b28552b63bb6e5181b8
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-11-08 17:22:21 +0100

    fix: release

--- src/__tests__/Input.test.tsx
+++ src/__tests__/Input.test.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { Input } from '../Input';
 
 describe('<Input />', () => {
